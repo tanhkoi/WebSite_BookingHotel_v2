032cf1d3ff0ccc1678c51f8179e758d838e07be1
tnahkoi <tankhoi46@example.com>
2023-08-03 09:44:53 +0700

    fix bug search

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -16,7 +16,7 @@ const List = () => {
 	const [options, setOptions] = useState(location.state.options);
 	const [min, setMin] = useState(undefined);
 	const [max, setMax] = useState(undefined);
-	const { data, loading, error, reFetch } = useFetch(`/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`);
+	const { data, loading, error, reFetch } = useFetch(`/hotels?city=${destination}&min=${min || 0}&max=${max || 999999999}`);
 
 	const handleClick = () => {
 		reFetch();
